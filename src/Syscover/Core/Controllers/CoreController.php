eff45e9500fd625cd3b8a485b18c47e92c8b53d8
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-04-03 21:14:45 +0200

    Core Controller

--- src/Syscover/Core/Controllers/CoreController.php
+++ src/Syscover/Core/Controllers/CoreController.php
@@ -14,4 +14,51 @@ abstract class CoreController extends BaseController
     {
 
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $objects = call_user_func($this->model . '::builder')->get();
+
+        $response['data'] = $objects;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param   string  $id
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $object = call_user_func($this->model . '::builder')->find($id);
+
+        $response['data'] = $object;
+
+        return response()->json($response);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param   string  $id
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $object = call_user_func($this->model . '::builder')->find($id);
+
+        $object->delete();
+
+        $response['status'] = "success";
+        $response['data']   = $object;
+
+        return response()->json($response);
+    }
 }
