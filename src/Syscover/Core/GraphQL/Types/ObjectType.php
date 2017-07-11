2060c0c7ea81e7acf04d97e7fa14b0eb68fb94b7
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-07-12 00:31:42 +0200

    Add ObjectType

---
+++ src/Syscover/Core/GraphQL/Types/ObjectType.php
@@ -0,0 +1,51 @@
+<?php namespace Syscover\Core\GraphQL\Types;
+
+use GraphQL\Type\Definition\ScalarType;
+
+use GraphQL\Error\Error;
+use GraphQL\Language\AST\StringValueNode;
+use GraphQL\Utils;
+
+class ObjectType extends ScalarType
+{
+
+    public $name = "Object2";
+    public $description = "Any object of application";
+
+    /**
+     * Serializes an internal value to include in a response.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function serialize($value)
+    {
+        return $value;
+    }
+
+    /**
+     * Parses an externally provided value (query variable) to use as an input
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    public function parseValue($value)
+    {
+        return $value;
+    }
+
+    public function parseLiteral($valueNode)
+    {
+        // Note: throwing GraphQL\Error\Error vs \UnexpectedValueException to benefit from GraphQL
+        // error location in query:
+        if (!$valueNode instanceof StringValueNode) {
+            throw new Error('Query error: Can only parse strings got: ' . $valueNode->kind, [$valueNode]);
+        }
+        if (!filter_var($valueNode->value, FILTER_VALIDATE_EMAIL)) {
+            throw new Error("Not a valid email", [$valueNode]);
+        }
+        return $valueNode->value;
+    }
+
+
+}
