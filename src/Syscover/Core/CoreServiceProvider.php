d47b820a93fdeb8da8267b82f442ee54a9eb8dd8
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-07-26 11:50:51 +0200

    add  routes to service provider

--- src/Syscover/Core/CoreServiceProvider.php
+++ src/Syscover/Core/CoreServiceProvider.php
@@ -12,6 +12,9 @@ class CoreServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
+        // register routes
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+
         // register config files
         $this->publishes([
             __DIR__ . '/../../config/pulsar.core.php' => config_path('pulsar.core.php'),
