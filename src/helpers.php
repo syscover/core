82d9cfcefa9091076e5bf08519c1b934205f4990
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-02 20:23:12 +0200

    add helper date_time_string

--- src/helpers.php
+++ src/helpers.php
@@ -14,3 +14,17 @@ if (! function_exists('trans_has')) {
         return app('translator')->has($key, $locale, $fallback);
     }
 }
+
+if (! function_exists('date_time_string')) {
+    /**
+     * Transform javascript date to timestamp
+     *
+     * @param  string  $value
+     * @return string
+     */
+    function date_time_string($value)
+    {
+        // use preg_replace to format date from Google Chrome, attach (Hota de verano romance) string
+        return (new \Carbon\Carbon(preg_replace('/\(.*\)/','', $value), config('app.timezone')))->toDateTimeString();
+    }
+}
