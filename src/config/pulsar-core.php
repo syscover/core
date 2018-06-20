6b4f9d6e14ade288a37f4d4cc7a45926507997cb
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-06-20 23:02:48 +0200

    add exif_fields_allowed

--- src/config/pulsar-core.php
+++ src/config/pulsar-core.php
@@ -14,4 +14,33 @@ return [
     //***
     //******************************************************************************************************************
     'datePattern'               => 'd-m-Y',
+
+    //******************************************************************************************************************
+    //***   Set fields to save from EXIT image properties to avoid utf-8 characters.
+    //***   That they are includes by software like Photoshop
+    //******************************************************************************************************************
+    'exif_fields_allowed'       => [
+        'FileName',
+        'FileDateTime',
+        'FileSize',
+        'FileType',
+        'MimeTye',
+        'SectionsFound',
+        'COMPUTED',
+        'ImageWidth',
+        'ImageLength',
+        'BitsPerSample',
+        'PhotometricInterpretation',
+        'ImageDescription',
+        'Orientation',
+        'SamplesPerPixel',
+        'XResolution',
+        'YResolution',
+        'ResolutionUnit',
+        'Software',
+        'DateTime',
+        'Title',
+        'Comments',
+        'Keywords'
+    ]
 ];
