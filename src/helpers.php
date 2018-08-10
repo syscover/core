e15a640e00d96f67abada7a0647d06ab31b3db86
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-08-10 11:09:25 +0200

    minor fix

--- src/helpers.php
+++ src/helpers.php
@@ -54,7 +54,7 @@ if (! function_exists('next_id')) {
     function next_id($model, $key = 'id')
     {
         $model  = new $model;
-        $id = $model->max($key);
+        $id     = $model->max($key);
         $id++;
 
         return $id;
