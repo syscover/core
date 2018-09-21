65d2184588b1dcf725dc8dd10e085a869f473a85
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-21 16:29:50 +0200

    add queries

--- src/Syscover/Core/GraphQL/Services/UserGraphQLService.php
+++ src/Syscover/Core/GraphQL/Services/UserGraphQLService.php
@@ -8,9 +8,19 @@ class UserGraphQLService
     {
         if(isset($args['guard']))
         {
-            return Auth::guard($args['guard'] ?? null)->user();
+            return Auth::guard($args['guard'])->user();
         }
 
         return Auth::user();
+    }
+
+    public function resolveCheck($root, array $args)
+    {
+        if(isset($args['guard']))
+        {
+            return Auth::guard($args['guard'])->check();
+        }
+
+        return Auth::check();
     }
 }
