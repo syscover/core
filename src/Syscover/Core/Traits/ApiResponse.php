e69f3c15d0997849f14ef073e924e400c09872ae
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-02-10 12:55:04 +0100

    - add ApiResponse - add ValidatesGraphQL

---
+++ src/Syscover/Core/Traits/ApiResponse.php
@@ -0,0 +1,33 @@
+<?php namespace Syscover\Core\Traits;
+
+use Illuminate\Http\Response;
+
+trait ApiResponse
+{
+    /**
+     * @param   $data
+     * @param   int $code
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json([
+            'status'        => $code,
+            'statusText'    => 'success',
+            'data'          => $data
+        ], $code);
+    }
+
+    /**
+     * @param   $message
+     * @param   $code
+     * @return  \Illuminate\Http\JsonResponse
+     */
+    public function errorResponse($message, $code)
+    {
+        return response()->json([
+            'status'        => $code,
+            'statusText'    => $message,
+        ], $code);
+    }
+}
